800a53e6cb562fbc0c92c24f724379f74e434e54
samjbro <sjbroughton1@gmail.com>
2016-06-22 12:59:00 +0100

    Section 13 completed

--- directory.rb
+++ directory.rb
@@ -1,22 +1,10 @@
 require 'date'
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :january, hobby: "Villainy", height: "Unknown"},
-  {name: "Darth Vader", cohort: :january, hobby: "Villainy", height: "Unknown"},
-  {name: "Nurse Ratched", cohort: :january, hobby: "Villainy", height: "Unknown"},
-  {name: "Michael Corleone", cohort: :january, hobby: "Villainy", height: "Unknown"},
-  {name: "Alex DeLarge", cohort: :november, hobby: "Villainy", height: "Unknown"},
-  {name: "The Wicked Witch of the West", cohort: :november, hobby: "Villainy", height: "Unknown"},
-  {name: "Terminator", cohort: :november, hobby: "Villainy", height: "Unknown"},
-  {name: "Freddy Krueger", cohort: :november, hobby: "Villainy", height: "Unknown"},
-  {name: "The Joker", cohort: :november, hobby: "Villainy", height: "Unknown"},
-  {name: "Joffrey Baratheon", cohort: :november, hobby: "Villainy", height: "Unknown"},  
-  {name: "Norman Bates", cohort: :november, hobby: "Villainy", height: "Unknown"}
-]
 @students = []
+
 def interactive_menu
 	loop do
 		print_menu
-		process(gets.chomp)
+		process(STDIN.gets.chomp)
 	end
 end
 
@@ -37,7 +25,7 @@ def process selection
 	when "3"
 		save_students
 	when "4"
-		load_students
+		try_load_students
 	when "9"
 		exit
 	else
@@ -45,33 +33,36 @@ def process selection
 	end
 end
 
-def show_students
-	print_header
-	print_students_list
-	print_footer
-end
-
 def input_student
-	cohort,hobby,height = :november, "unknown", "unknown"
+	#cohort,hobby,height = :november, "unknown", "unknown"
 	puts "Please enter the student's NAME/COHORT/HOBBY/HEIGHT"
-	input = gets.chomp.split('/')
+	input = STDIN.gets.chomp.split('/')
 	return @students if input.empty?
 	name = input[0]
-	cohort = input[1] unless input[1] == nil
+	input[1] = :none if input[1] == nil
+	cohort = check_valid_month input[1]
 	hobby = input[2] unless input[2] == nil
 	height = input[3] unless input[3] == nil
-	if !Date::MONTHNAMES[1..-1].include? cohort.capitalize
-		puts "Please re-enter the student's cohort"
-		cohort = gets.chomp
-		if !Date::MONTHNAMES[1..-1].include? cohort.capitalize
-			cohort = :november
-			puts "Invalid input - default of 'November' assigned"
-		end
-	end
+	
 	@students << {name: name, cohort: cohort.to_sym, height: height, hobby: hobby}
 	plural = ""
 	plural = "s" if (@students.count > 1)
 	puts "Now we have #{@students.count} student#{plural}"
+end
+
+def check_valid_month cohort
+	return cohort unless !Date::MONTHNAMES[1..-1].include? cohort.capitalize
+	puts "Please re-enter the student's cohort"
+	cohort = STDIN.gets.chomp
+	cohort = :none if cohort == nil
+	check_valid_month cohort
+end
+
+def show_students
+	cohort = choose_cohort
+	print_header cohort
+	print_students_list cohort
+	print_footer
 end
 
 def save_students
@@ -84,8 +75,19 @@ def save_students
 	file.close
 end
 
-def load_students
-	file = File.open("students.csv", "r")
+def try_load_students
+	filename = ARGV.first
+	return if filename.nil?
+	if File.exists?(filename)
+		load_students(filename)
+		puts "Loaded #{@students.count} from #{filename}"
+	else
+		abort "Sorry, #{filename} doesn't exist."
+	end
+end
+
+def load_students(filename = "students.csv")
+	file = File.open(filename, "r")
 	file.readlines.each do |line|
 		name, cohort, hobby, height = line.chomp.split(',')
 		@students << {name: name, cohort: cohort.to_sym, hobby: hobby, height: height}
@@ -93,14 +95,18 @@ def load_students
 	file.close
 end
 
-def print_header cohort=:november
+def print_header cohort="2016"
 	puts "The students of Villains Academy's #{cohort.capitalize} cohort".center(85)
 	puts "-------------".center(85)
 end
 
-def print_students_list cohort=:november, letters="a".."z", character_max=100
-	names = @students.select{|x| x[:cohort].downcase == cohort.to_sym.downcase}
-	(return puts "No students are on this cohort".center(85)) if names.empty?
+def print_students_list cohort="2016", letters="a".."z", character_max=100
+	if cohort == "2016"
+		names = @students
+	else
+		names = @students.select{|x| x[:cohort].downcase == cohort.to_sym.downcase}
+	end
+	(return puts "No students are in this cohort".center(85)) if names.empty?
 	i = 0
 	while i < names.length
 		puts "#{i+1}. #{names[i][:name]} (Cohort: #{names[i][:cohort].capitalize}, Hobby: #{names[i][:hobby]}, Height: #{names[i][:height]})".center(85) if ((letters.include? names[i][:name][0].downcase) && (names[i][:name].length < character_max))
@@ -116,14 +122,9 @@ end
 
 def choose_cohort
 	puts "Which month's cohort would you like to see?"
-	gets.chomp
+	puts "Enter '2016' to see the year's full cohort"
+	STDIN.gets.chomp
 end
 
-# @students = input_@students
-# cohort = "november"
-# cohort = choose_cohort
-# print_header(cohort)
-# print(@students, cohort,)#"d", 12)
-# print_footer(@students, cohort)
 interactive_menu
 
